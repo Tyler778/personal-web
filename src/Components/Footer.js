7dea9fe45452438d8042a0dcf844efcaa1024f48
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2022-02-01 15:58:16 -0500

    small appearance and link changes for easier use of website

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,12 +23,9 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>&copy; Copyright 2021 Nordic Giant</li>
+                <li>&copy; Copyright 2021 Tyler Quante</li>
                 <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
+                  Design by Tyler Quante
                 </li>
               </ul>
             </div>
